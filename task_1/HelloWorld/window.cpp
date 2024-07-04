0ac3e9cdebb1be45a06baa1ba681db558be8ffbf
Henrik Lindblom <henrik.lindblom@regionvasterbotten.se>
2024-07-04 19:26:07 +0200

    slider and progress bar example

--- task_1/HelloWorld/window.cpp
+++ task_1/HelloWorld/window.cpp
@@ -6,12 +6,12 @@
 Window::Window(QWidget *parent) :
  QWidget(parent)
  {
- // Set size of the window
- setFixedSize(100, 50);
+    // Set size of the window
+    // setFixedSize(100, 50);
 
- // Create and position the button
- m_button = new QPushButton("Hello World", this);
- m_button->setGeometry(10, 10, 80, 30);
+    // Create and position the button
+    m_button = new QPushButton("Exit", this);
+    m_button->setGeometry(200, 5, 80, 30);
 
- connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
+    connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
 }
